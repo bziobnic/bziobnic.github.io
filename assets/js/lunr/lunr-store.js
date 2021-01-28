ecab483541ea2a6a707d99a7231f06c0c4daffe2
Scott Zionic <scott@zionic.net>
2021-01-28 13:03:37 -0500

    initial commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes, etc, etc. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2021/01/21/welcome-to-jekyll.html",
+        "teaser": null
+      }]
